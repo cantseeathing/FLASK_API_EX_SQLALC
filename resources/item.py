ff206374388aedab7902d2b7348f0610bd06b3ed
cantseeathing <mahmoued_999@hotmail.com>
2023-09-09 21:54:55 +0300

    first commit

---
+++ resources/item.py
@@ -0,0 +1,60 @@
+import uuid
+from flask import request
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from schemas import ItemSchema, ItemUpdateSchema
+from models import ItemModel
+from db import db
+from sqlalchemy.exc import SQLAlchemyError
+
+blp = Blueprint("items", __name__, description="Operations on items")
+
+
+@blp.route('/items/<string:item_id>')
+class Item(MethodView):
+    @blp.response(200, ItemSchema)
+    def get(self, item_id):
+        item = ItemModel.query.get_or_404(item_id)
+        return item, 200
+
+    @blp.arguments(ItemUpdateSchema)
+    @blp.response(201, ItemSchema)
+    def put(self, item_data, item_id):
+        item = ItemModel.query.get(item_id)
+        if item:
+            item.price = item_data['price']
+            item.name = item_data['name']
+        else:
+            item = ItemModel(id=item_id, **item_data)
+        db.session.add(item)
+        db.session.commit()
+
+        return item
+        # raise NotImplementedError("Updating an Item is not implemented!")
+
+    def delete(self, item_id):
+        item = ItemModel.query.get_or_404(item_id)
+        db.session.delete(item)
+        db.session.commit()
+        return {"message": "Item deleted."}, 200
+
+
+@blp.route('/items')
+class ItemList(MethodView):
+    @blp.response(200, ItemSchema(many=True))
+    def get(self):
+        return ItemModel.query.all()
+
+    @blp.arguments(ItemSchema)
+    @blp.response(201, ItemSchema)
+    def post(self, item_data):
+        # if item_data["store_id"] not in stores:
+        #     abort(404, message="Store not found!")
+        #     # return {"message": "Store not found!"}, 404
+        item = ItemModel(**item_data)
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occurred when inserting the item to the DB!")
+        return item, 201
